a4daf5ee2db8076dca6cd322538f0f08394d4ccd
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2024-04-20 01:21:00 +0200

    More Arrays

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 `go run` command compiles the code into a temporary directory, and then deletes it after the program finished.
 `go build` creates executable file, that can be used to be run later.
 
+## The Zero Value
+Go has a concept of zero value. When a variable is declared, it is assigned a zero value. The zero value is:
+- 0 for numeric types
+- false for the boolean type
+- "" for strings
+- nil for interfaces, slices, channels, maps, pointers and functions
--- hello.go
+++ hello.go
@@ -3,5 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	var greetings = `Greetings and
+					 "salutations!""`
+	var flag = true
+	// Complex number
+	x := 1 + 2i
+	y := 2 + 3i
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(x + y)
+	fmt.Println(x - y)
+	fmt.Println(x * y)
+	fmt.Println(x / y)
+	fmt.Println(real(x))
+	fmt.Println(imag(x))
+	// constants
+	const name = "Hassan Elseoudy"
+	fmt.Println(name)
+	// arrays
+	var arr = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(arr)
+	fmt.Println(flag)
+	fmt.Println(greetings)
 }

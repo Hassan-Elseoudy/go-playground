083f555d088f0e6cabfadeae8d2a01ea4223ec9f
Hassan El-seoudy <hassan.elseoudy@gmail.com>
2024-04-20 01:41:52 +0200

    More slices

--- hello.go
+++ hello.go
@@ -25,6 +25,7 @@ func main() {
 	fmt.Println(arr)
 	// slices
 	var slice = []int{1, 2, 3, 4, 5}
+	fmt.Println(slice)
 	var slice2 = []int{6, 7, 8, 9, 10}
 	// append to slice
 	slice = append(slice, 6)
@@ -32,7 +33,21 @@ func main() {
 	slice = append(slice, slice2...)
 	// print capacity of slice, and length of slice
 	fmt.Println(cap(slice), len(slice))
-	fmt.Println(slice)
+	// slice literal
+	slice3 := []int{1, 2, 3, 4, 5}
+	slice3 = append(slice3, 6)
+	fmt.Println(slice3)
+	// slices share the same memory
+	slice4 := slice3
+	slice4[0] = 100
+	fmt.Println(slice3)
+	// copy slices
+	slice5 := make([]int, len(slice3))
+	copy(slice5, slice3)
+	slice5[0] = 200
+	fmt.Println(slice3)
+	fmt.Println(slice5)
+
 	fmt.Println(flag)
 	fmt.Println(greetings)
 }
